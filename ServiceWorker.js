d1fda048ca4b40aaf932231d6b45d3ab37e9b938
Muhammad Hammad Arshad <ihammadarshad@gmail.com>
2026-03-10 21:40:02 +0500

    New version - 10-03-2026

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "EXA Labs-Exaverse-0.6";
+const cacheName = "EXA Labs-Exaverse-0.6.1";
 const contentToCache = [
-    "Build/Exaverse Full Version WebGL Build-V(0.6) 06-March-2026.loader.js",
-    "Build/Exaverse Full Version WebGL Build-V(0.6) 06-March-2026.framework.js",
-    "Build/Exaverse Full Version WebGL Build-V(0.6) 06-March-2026.data",
-    "Build/Exaverse Full Version WebGL Build-V(0.6) 06-March-2026.wasm",
+    "Build/Exaverse Full Version WebGL Build-V(0.6.1) 10-March-2026.loader.js",
+    "Build/Exaverse Full Version WebGL Build-V(0.6.1) 10-March-2026.framework.js",
+    "Build/Exaverse Full Version WebGL Build-V(0.6.1) 10-March-2026.data",
+    "Build/Exaverse Full Version WebGL Build-V(0.6.1) 10-March-2026.wasm",
     "TemplateData/style.css"
 
 ];
